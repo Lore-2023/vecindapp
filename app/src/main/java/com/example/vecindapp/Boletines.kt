5646b72b5007169a2251c23554302c3093d1ce47
DavidR0813 <dh.rodriguez@urepublicana.edu.co>
2023-11-25 17:57:17 -0500

    Prueba de pantalla boletines

---
+++ app/src/main/java/com/example/vecindapp/Boletines.kt
@@ -0,0 +1,11 @@
+package com.example.vecindapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Boletines : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boletines)
+    }
+}
